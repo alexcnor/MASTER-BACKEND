64418eb82096fdd9eef47ba049bb5f42cf600bbc
Ismael Leiva <ismael.leiva@tecnoandina.cl>
2022-10-02 19:23:29 -0300

    fix fake test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -6,20 +6,28 @@ import { HttpModule } from '@nestjs/axios';
 
 describe('AppController', () => {
   let appController: infrastructureController;
-
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       imports: [HttpModule],
       controllers: [infrastructureController],
-      providers: [InfrastructureService],
+      providers: [
+        {
+          provide: InfrastructureService,
+          useValue: {
+            createAKS: () => true,
+          },
+        },
+      ],
     }).compile();
 
     appController = app.get<infrastructureController>(infrastructureController);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.create(new InfraDto())).toBe('');
+    it('should return true"', () => {
+      const dto = new InfraDto();
+      dto.componente = 'kubernetes cluster';
+      expect(appController.create(dto)).toBe(true);
     });
   });
 });
